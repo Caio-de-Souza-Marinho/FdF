c01cee99375ad968fd8ec0332eaf7ab198b7f81c
Caio De Souza Marinho <caide-so@c1r4p3.42sp.org.br>
2024-12-23 19:34:00 -0300

    study: close the window when press the ESC key

--- main.c
+++ main.c
@@ -1,19 +1,31 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include "minilibx-linux/mlx.h"
 
-int	main(void)
+#define ESC_KEY 65307 // Key code for ESC on most systems
+
+typedef struct s_vars
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+} 	t_vars;
 
-	// init x server
-	mlx_ptr = mlx_init();
+int	deal_key(int key, void *param);
+void	ft_putstr(char *str);
 
+int	main(void)
+{
+	t_vars vars;
+
+	// Initialize MiniLibX
+	vars.mlx_ptr = mlx_init(); // init x server
+	//
+	// open window
 	int win_x = 200;
 	int win_y = 200;
-	// open window
-	win_ptr = mlx_new_window(mlx_ptr, win_x, win_y, "mlx study");
+	vars.win_ptr = mlx_new_window(vars.mlx_ptr, win_x, win_y, "mlx study");
 	
-	// testing colors
+	// colors
 	int red = 255;
 	int green = 0;
 	int blue = 0;
@@ -25,22 +37,49 @@ int	main(void)
 	int i = 0 ;
 	while (i < 200)
 	{
-		mlx_pixel_put(mlx_ptr, win_ptr, v_line_x, v_line_y, color);
+		mlx_pixel_put(vars.mlx_ptr, vars.win_ptr, v_line_x, v_line_y, color);
 		v_line_x++;
 		i++;
 	}
 
-	// draw a vertical line 
+	// draw a horizontal line 
 	int pixel_start_x = 100;
 	int pixel_start_y = 0;
 	i = 0 ;
 	while (i < 200)
 	{
-		mlx_pixel_put(mlx_ptr, win_ptr, pixel_start_x, pixel_start_y, color);
+		mlx_pixel_put(vars.mlx_ptr, vars.win_ptr, pixel_start_x, pixel_start_y, color);
 		pixel_start_y++;
 		i++;
 	}
+
+	// key hook event - press a key to trigger an event
+	mlx_key_hook(vars.win_ptr, deal_key, &vars);
+
 	// handle window events and drawing
-	mlx_loop(mlx_ptr);
+	mlx_loop(vars.mlx_ptr);
 	return (0);
+}
+
+int	deal_key(int key, void *param)
+{
+	t_vars *vars = (t_vars *)param;
+
+	if (key == ESC_KEY)
+	{
+		mlx_destroy_window((void *)vars->mlx_ptr, vars->win_ptr);
+		ft_putstr("ESC pressed\n");
+		exit(0);
+	}
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
