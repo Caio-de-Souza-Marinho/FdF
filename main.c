0779853d897a3fe88cc8ea7af4ad571b07d52486
Caio De Souza Marinho <caide-so@c1r4p3.42sp.org.br>
2024-12-23 18:28:33 -0300

    study: can now draw two lines, one vertical and one horizontal

--- main.c
+++ main.c
@@ -8,19 +8,38 @@ int	main(void)
 	// init x server
 	mlx_ptr = mlx_init();
 
+	int win_x = 200;
+	int win_y = 200;
 	// open window
-	win_ptr = mlx_new_window(mlx_ptr, 50, 50, "nice");
-
+	win_ptr = mlx_new_window(mlx_ptr, win_x, win_y, "mlx study");
 	
 	// testing colors
 	int red = 255;
 	int green = 0;
-	int blue = 127;
+	int blue = 0;
 	int color = (red << 16) | (green << 8) | blue;
 
-	// draw pixel
-	mlx_pixel_put(mlx_ptr, win_ptr, 25, 25, color);
+	// draw a vertical line 
+	int v_line_x = 0;
+	int v_line_y = 100;
+	int i = 0 ;
+	while (i < 200)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, v_line_x, v_line_y, color);
+		v_line_x++;
+		i++;
+	}
 
+	// draw a vertical line 
+	int pixel_start_x = 100;
+	int pixel_start_y = 0;
+	i = 0 ;
+	while (i < 200)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, pixel_start_x, pixel_start_y, color);
+		pixel_start_y++;
+		i++;
+	}
 	// handle window events and drawing
 	mlx_loop(mlx_ptr);
 	return (0);
