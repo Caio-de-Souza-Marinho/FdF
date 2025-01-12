82f20f45b605093d91141a245f550967f0eaab0f
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2025-01-12 03:12:37 -0300

    feature: add continuous action handling

--- main.c
+++ main.c
@@ -11,9 +11,7 @@
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "lib/minilibx-linux/mlx.h"
 
-static int	expose_handle(t_fdf *fdf);
 static int	close_window(t_fdf *fdf);
 
 int	main(int argc, char *argv[])
@@ -26,21 +24,60 @@ int	main(int argc, char *argv[])
 	file_name = argv[1];
 	fdf = init_fdf(file_name);
 	render(fdf);
-	mlx_key_hook(fdf->win, &key_handle, fdf);
-	mlx_expose_hook(fdf->win, &expose_handle, fdf);
+	mlx_hook(fdf->win, 2, 1L << 0, &key_press, fdf);
+	mlx_hook(fdf->win, 3, 1L << 1, &key_release, fdf);
 	mlx_hook(fdf->win, 17, 0, &close_window, fdf);
+	mlx_loop_hook(fdf->mlx, &loop_hook, fdf);
 	mlx_loop(fdf->mlx);
-	return (0);
-}
-
-static int	expose_handle(t_fdf *fdf)
-{
-	render(fdf);
 	return (0);
 }
 
 static int	close_window(t_fdf *fdf)
 {
 	close_all(fdf, 0);
+	return (0);
+}
+
+int	loop_hook(t_fdf *fdf)
+{
+	if (fdf->keys[KEY_ESC])
+		close_all(fdf, 0);
+	else if (fdf->keys[KEY_RIGHT] || fdf->keys[KEY_LEFT] || fdf->keys[KEY_UP]
+		|| fdf->keys[KEY_DOWN])
+		key_translate(-1, fdf);
+	else if (fdf->keys[KEY_PLUS] || fdf->keys[KEY_MINUS] || fdf->keys[KEY_Z]
+		|| fdf->keys[KEY_X])
+		key_scale(-1, fdf);
+	else if (fdf->keys[KEY_A] || fdf->keys[KEY_S] || fdf->keys[KEY_D]
+		|| fdf->keys[KEY_Q] || fdf->keys[KEY_W] || fdf->keys[KEY_E])
+		key_rotate(-1, fdf);
+	render(fdf);
+	return (0);
+}
+
+int	key_press(int keycode, t_fdf *fdf)
+{
+	if (keycode >= 0 && keycode < MAX_KEYS)
+	{
+		fdf->keys[keycode] = 1;
+		if (keycode == KEY_P || keycode == KEY_I || keycode == KEY_O)
+			key_project(keycode, fdf);
+		else if (keycode == KEY_SPACE)
+		{
+			if (fdf->cam->color_pallet == FALSE)
+				fdf->cam->color_pallet = TRUE;
+			else
+				fdf->cam->color_pallet = FALSE;
+		}
+		else if (keycode == KEY_R)
+			reset(fdf);
+	}
+	return (0);
+}
+
+int	key_release(int keycode, t_fdf *fdf)
+{
+	if (keycode >= 0 && keycode < MAX_KEYS)
+		fdf->keys[keycode] = 0;
 	return (0);
 }
